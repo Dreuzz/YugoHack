037fe91d6f8142c74ccfe5c0e9b7112344d97143
Dreuzzz <dreuzzz@proton.me>
2024-01-02 17:59:14 +0100

    Initial commit

---
+++ src/main/java/net/dreuzzz/yugohack/YugohackClient.java
@@ -0,0 +1,10 @@
+package net.dreuzzz.yugohack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YugohackClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
